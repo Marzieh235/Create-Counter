2d2ac593bb0a9fd80612d70349d2791c3be6a327
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-01 17:01:59 +0330

    Rewriting and improving the code

--- script.js
+++ script.js
@@ -1,52 +1,73 @@
 let startbox = document.querySelector('.start-box');
 let inputCounter = document.querySelector('#input-counter');
 let startCounter = document.querySelector('#start-counter');
-let ErrorElement = document.querySelector('#error-message');
 let TimeCircle = document.querySelector('.c100');
 let TimerNum = document.querySelector('.c100 > span');
-let LoadingMessage = document.querySelector('.message .loading');
-let SuccessMessage = document.querySelector('.message .success');
-
-
+let lastpercent = 'p100';
+let seconds, originalSeconds, timerId
 
 startCounter.addEventListener('click', function (e) {
-    let seconds = parseInt(inputCounter.value)
-    if (isNaN(seconds)) {
-        ErrorElement.textContent = 'زمان را به درستی وارد کنید.';
-        ErrorElement.classList.add('active')
+    seconds = parseInt(inputCounter.value)
+    if (isNaN(seconds)) return toggleErrormessage({ show: true, message: 'زمان را به درستی وارد کنید.' });
 
+    toggleErrormessage({ show: false });
+    togglestartbox({ show: false });
+    toggleloadingmessage({ show: true });
+    toggleTimer({ show: true, seconds });
+
+    originalSeconds = seconds;
+    timerId = setInterval(startTimer, 1000);
+})
+let startTimer = () => {
+    if (lastpercent) {
+        TimeCircle.classList.remove(lastpercent)
+    } if (seconds <= 0) {
+        clearInterval(timerId)
+        togglestartbox({ show: true });
+        toggleTimer({ show: false });
+        toggleloadingmessage({ show: false });
         return;
     }
-
-    startbox.classList.remove('active');
-    ErrorElement.classList.remove('active');
-    TimeCircle.style.display = 'block';
+    seconds -= 1;
     TimerNum.textContent = seconds;
-    LoadingMessage.style.display = 'block';
-    SuccessMessage.style.display = 'none';
 
-    let originalSeconds = seconds;
-    let lastpercent = 'p100';
-    let timerId = setInterval(() => {
-        if (lastpercent) {
-            TimeCircle.classList.remove(lastpercent)
-        } if (seconds <= 0) {
-            clearInterval(timerId)
-            startbox.classList.add('active');
-            TimeCircle.style.display = 'none';
-            LoadingMessage.style.display = 'none';
-            SuccessMessage.style.display = 'block';
-            inputCounter.value = '';
-            return;
-        }
+    let percent = lastpercent = `p${Math.abs(Math.floor((((originalSeconds - seconds) / originalSeconds) * 100) - 100))}`
+    TimeCircle.classList.add(percent);
+}
+let toggleErrormessage = ({ show, message }) => {
+    let ErrorElement = document.querySelector('#error-message');
+    if (show) {
+        ErrorElement.textContent = message;
+        ErrorElement.classList.add('active');
+    } else {
+        ErrorElement.classList.remove('active');
+    }
+}
+let togglestartbox = ({ show }) => {
+    if (show) {
+        startbox.classList.add('active');
+        inputCounter.value = '';
+    } else {
+        startbox.classList.remove('active');
+    }
+}
+let toggleloadingmessage = ({ show }) => {
+    let LoadingMessage = document.querySelector('.message .loading');
+    let SuccessMessage = document.querySelector('.message .success');
+    if (show) {
+        LoadingMessage.style.display = 'block';
+        SuccessMessage.style.display = 'none';
 
-
-        seconds -= 1;
-        let percent = Math.abs(Math.floor((((originalSeconds - seconds) / originalSeconds) * 100)-100));
-        TimeCircle.classList.add(`p${percent}`);
-        lastpercent = `p${percent}`;
-
+    } else {
+        LoadingMessage.style.display = 'none';
+        SuccessMessage.style.display = 'block';
+    }
+}
+let toggleTimer = ({ show, seconds }) => {
+    if (show) {
+        TimeCircle.style.display = 'block';
         TimerNum.textContent = seconds;
-    }, 1000);
-})
-
+    } else {
+        TimeCircle.style.display = 'none';
+    }
+}
