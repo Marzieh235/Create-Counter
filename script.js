011c49da51bd90934b17d967391f4e39fc22e5e7
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-01 15:18:24 +0330

    complete TIMER

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let startCounter = document.querySelector('#start-counter');
 let ErrorElement = document.querySelector('#error-message');
 let TimeCircle = document.querySelector('.c100');
 let TimerNum = document.querySelector('.c100 > span');
+let LoadingMessage = document.querySelector('.message .loading');
+let SuccessMessage = document.querySelector('.message .success');
 
 
 
@@ -16,13 +18,24 @@ startCounter.addEventListener('click', function (e) {
         return;
     }
 
-    startbox.style.display = 'none';
+    startbox.classList.remove('active');
     ErrorElement.classList.remove('active');
     TimeCircle.style.display = 'block';
     TimerNum.textContent = seconds;
+    LoadingMessage.style.display = 'block';
+    SuccessMessage.style.display = 'none';
 
 
-    setInterval(() => {
+    let timerId = setInterval(() => {
+        if(seconds <= 1){
+            clearInterval(timerId)
+            startbox.classList.add('active');
+            TimeCircle.style.display = 'none';
+            inputCounter.value = '';
+            LoadingMessage.style.display = 'none';
+            SuccessMessage.style.display = 'block';
+
+        }
         seconds -= 1;
 
         TimerNum.textContent = seconds;
